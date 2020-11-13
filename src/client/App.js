9ff38b3583791ae781943d51b17969543ba1cdb8
Vova Afan <grmh9ko@gmail.com>
2020-11-13 16:24:33 +0300

    fix webpack

--- src/client/App.js
+++ src/client/App.js
@@ -7,8 +7,8 @@ export default class App extends Component {
 
   componentDidMount() {
     fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+      .then((res) => res.json())
+      .then((user) => this.setState({ username: user.username }));
   }
 
   render() {
