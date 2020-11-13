1fde0953304db11051d0855931b87385d76cd76e
Vova Afan <grmh9ko@gmail.com>
2020-11-13 17:50:53 +0300

    work version

--- src/client/App.js
+++ src/client/App.js
@@ -7,8 +7,8 @@ export default class App extends Component {
 
   componentDidMount() {
     fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+      .then((res) => res.json())
+      .then((user) => this.setState({ username: user.username }));
   }
 
   render() {
@@ -16,7 +16,6 @@ export default class App extends Component {
     return (
       <div>
         {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
       </div>
     );
   }
